2a6ef17043cc2e49fc6296f8361b42f739779fc1
Даниил Ларочкин <daniil@MacBook-Air-Daniil-4.local>
2024-12-22 23:14:17 +0300

    Сохранение проекта

--- src/components/ProductSpecifications/ProductSpecifications.jsx
+++ src/components/ProductSpecifications/ProductSpecifications.jsx
@@ -1,9 +1,30 @@
-// import styles from "./productSpecifications.module.scss";
+import React from "react";
+
+import { Data } from "../../App";
+
+import styles from "./productSpecifications.module.scss";
 
 export function ProductSpecifications() {
+  const { productСard } = React.useContext(Data);
+
   return (
-    <>
-      <>Характеристики</>
-    </>
+    <div className={styles.main}>
+      <h1 className={styles.title}>Характеристики</h1>
+      <div className={styles.container}>
+        {productСard.specifications.map((obj, index) => (
+          <div key={index} className={styles.specifications}>
+            <h2>{obj.title}</h2>
+
+            {obj.parameters.map((el, index) => (
+              <div key={index} className={styles.parameters}>
+                <p>{el.title}</p>
+                <div></div>
+                <p>{el.parameter}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
