9bd084f0609a7caba0a0761f32ae21e3b1ffe43f
Даниил Ларочкин <daniil@MacBook-Air-Daniil-4.local>
2025-01-08 15:55:59 +0300

    исправление ошибок

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,22 +5,13 @@ import styles from "./pagination.module.scss";
 
 export function Pagination({ data, dataLimit }) {
   const [itemOffset, setItemOffset] = React.useState(0);
-  const [active, setActive] = React.useState(0);
 
   const endOffset = itemOffset + dataLimit;
-
   const currentItems = data().slice(itemOffset, endOffset);
-
   const numberPages = Math.ceil(data().length / dataLimit);
 
   function onClickNumber(event) {
-    // if (active === event.selected) {
-
-    // }
-
-    console.log(event);
     const newOffset = (event.selected * dataLimit) % data().length;
-
     setItemOffset(newOffset);
   }
 
